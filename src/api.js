56f75b4673f9e8036833ca2aa29ac20d4842967b
Arda Satata <ardasatata@gmail.com>
2021-10-24 13:47:15 +0800

    init

---
+++ src/api.js
@@ -0,0 +1,31 @@
+import { create } from 'apisauce'
+
+export class Api {
+
+    apisauce;
+
+    constructor() {
+
+    }
+
+    setup() {
+        this.apisauce = create({
+            baseURL: 'http://140.115.51.225:5000/',
+            timeout: 60000
+        })
+    }
+
+    async getFilelist() {
+        const response = await this.apisauce.get('/file-path')
+        return response.data;
+    }
+
+    async getPrediction(filepath) {
+        console.log(filepath)
+        const response = await this.apisauce.get('/predict',{
+            filepath: filepath[0]
+        })
+        return response.data;
+    }
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  important: true,
+  //Purging for Production is configured in PostCSS Config
+  purge:{
+    content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {
+    backgroundColor: ['responsive', 'odd', 'hover', 'focus'],
+  },
+  plugins: [],
+};
