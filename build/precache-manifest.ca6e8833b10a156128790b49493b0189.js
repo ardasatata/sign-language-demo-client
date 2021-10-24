05c19b5804b2979365fef69d6ff5124d644121b7
Arda Satata <ardasatata@gmail.com>
2021-10-24 13:59:58 +0800

    latest build

---
+++ build/precache-manifest.ca6e8833b10a156128790b49493b0189.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e827723851e217b8447285b0212664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9152b119df8867ba3b11",
+    "url": "/static/css/main.b249bfb9.chunk.css"
+  },
+  {
+    "revision": "8a8556a0a1c762b7783f",
+    "url": "/static/js/2.81f1b637.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.81f1b637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9152b119df8867ba3b11",
+    "url": "/static/js/main.d3cb9460.chunk.js"
+  },
+  {
+    "revision": "6613802de99d406509fd",
+    "url": "/static/js/runtime-main.8d5285fa.js"
+  },
+  {
+    "revision": "d70f24980b7a93c15be0ba9eb95c6136",
+    "url": "/static/media/model.d70f2498.png"
+  }
+]);
